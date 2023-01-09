5bfb72f8ff3e52cb71fe960d8a97e315d45985d6
Ivan Tuzita <ivantuzita@gmail.com>
2023-01-09 10:57:43 -0300

    Adicionar arquivos de projeto.

---
+++ FridgeAPI/Controllers/FridgeController.cs
@@ -0,0 +1,30 @@
+﻿using FridgeAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FridgeAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FridgeController: ControllerBase
+{
+    private static List<Ingredient> ingredients = new List<Ingredient>();
+    private static int id = 0;
+
+    [HttpPost]
+    public IActionResult addIngredient([FromBody] Ingredient ingredient) {
+        ingredient.Id = id++;
+        ingredients.Add(ingredient);
+        return CreatedAtAction(nameof(getIngredientById), new {id = ingredient.Id}, ingredient);
+    }
+
+    [HttpGet]
+    public IEnumerable<Ingredient> getIngredients() { return ingredients; }
+
+    [HttpGet("{id}")]
+    public IActionResult getIngredientById(int id) {
+        var ingredient = ingredients.FirstOrDefault(Ingredient => Ingredient.Id == id);
+        if (ingredient == null) return NotFound();
+        return Ok(ingredient);
+    }
+}
---
+++ FridgeAPI/Models/Ingredient.cs
@@ -0,0 +1,7 @@
+﻿namespace FridgeAPI.Models; 
+public class Ingredient
+{
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public int Quantity { get; set; }
+}
